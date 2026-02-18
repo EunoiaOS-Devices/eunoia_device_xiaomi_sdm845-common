LineageOS/android_device_xiaomi_sdm845-common

sdm845-common: doze: Make AOD preference not persistent
Change-Id: I9d80e94de02d35c85765302a01b53020824dd0ff

parts/src/org/lineageos/settings/doze/DozeSettingsFragment.java
@@ -69,6 +69,7 @@ public class DozeSettingsFragment extends PreferenceFragment implements OnPrefer
 
         mAlwaysOnDisplayPreference = (SwitchPreference) findPreference(DozeUtils.ALWAYS_ON_DISPLAY);
         mAlwaysOnDisplayPreference.setEnabled(dozeEnabled);
+        mAlwaysOnDisplayPreference.setChecked(Utils.isAlwaysOnEnabled(getActivity()));
         mAlwaysOnDisplayPreference.setOnPreferenceChangeListener(this);
 
         PreferenceCategory pickupSensorCategory = (PreferenceCategory) getPreferenceScreen().

EunoiaOS-Devices/eunoia_device_xiaomi_sdm845-common

parts/src/org/lineageos/settings/doze/DozeSettingsFragment.java
@@ -69,6 +69,7 @@ public class DozeSettingsFragment extends PreferenceFragment implements OnPrefer
 
         mAlwaysOnDisplayPreference = (SwitchPreference) findPreference(DozeUtils.ALWAYS_ON_DISPLAY);
         mAlwaysOnDisplayPreference.setEnabled(dozeEnabled);
+        mAlwaysOnDisplayPreference.setChecked(DozeUtils.isAlwaysOnEnabled(getActivity()));
         mAlwaysOnDisplayPreference.setOnPreferenceChangeListener(this);
 
         PreferenceCategory pickupSensorCategory = (PreferenceCategory) getPreferenceScreen().